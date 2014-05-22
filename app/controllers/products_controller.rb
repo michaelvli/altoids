a39a50b323b8f87a628bf612e26cd1269b615515
Vee Li <michaelvli@yahoo.com>
2014-05-22 10:58:35 -0500

    added content to .js and .css manifest files

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,35 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.order("name").page(params[:page]).per_page(2)
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product, notice: "Successfully created product."
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_url, notice: "Successfully destroyed product."
+  end
+  
+  
+  private
+  
+	def product_params
+	    params.require(:product).permit(:name, :price, :released_at)
+	end
+end
