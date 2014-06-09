802242045a419d4182c411bb2eb7b0e3eeac7830
Vee Li <michaelvli@yahoo.com>
2014-06-08 23:26:23 -0500

    postgresql compatibility

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,8 @@ class SessionsController < ApplicationController
 		# Setting up activerecord relation between venues and neighborhoods is set up to sort by venue name and neighborhood
 #		@venues = Venue.select("venues.id, venues.name, venues.phone, venues.neighborhood_id, venues.file_name").joins(:neighborhood)
 #		@venues = Venue.select("DISTINCT(venues.name), venues.*, neighborhoods.*").joins(:neighborhood)
-		@venues = Venue.select("venues.*, neighborhoods.*, venue_events.*").joins(:neighborhood).joins('LEFT OUTER JOIN venue_events ON venues.id = venue_events.venue_id')
+#		@venues = Venue.select("venues.*, neighborhoods.*, venue_events.*").joins(:neighborhood).joins('LEFT OUTER JOIN venue_events ON venues.id = venue_events.venue_id')
+		@venues = Venue.select("venues.id, venues.name, venues.phone, venues.neighborhood_id, venues.file_name, neighborhoods.id, neighborhoods.name").joins(:neighborhood).joins('LEFT OUTER JOIN venue_events ON venues.id = venue_events.venue_id')
 		
 		# if latitude and longitude parameters are available, show distance from venues to user
 		if (params.has_key?(:latitude) && !params[:latitude].blank? && params.has_key?(:longitude) && !params[:longitude].blank?)
@@ -81,6 +82,7 @@ class SessionsController < ApplicationController
 			"distance"
 		else	
 #			"venue_events.id is null, venue_events.start_time asc" # sorts results by most recent start_times first followed by null (and by distance if available)
+			"venues.name asc"
 		end	
 	end	
 end
