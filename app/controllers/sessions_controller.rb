0d639f5288fb745126e194d5036282e7e998f01d
Vee Li <michaelvli@yahoo.com>
2014-06-21 15:35:54 -0500

    postgresql compatibility

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -42,10 +42,14 @@ class SessionsController < ApplicationController
 #ORDER BY subquery.start_time asc
 
 			# NOTE: DISTINCT ON IS ONLY FOR POSTGRESQL
-			@venues = Venue.select("DISTINCT ON(venues.id) venues.name as venue_name, venues.phone, venues.file_name, venue_events.id, venue_events.name as venue_event_name, venue_events.description as venue_event_description, venue_events.start_time")		
-			@venues = @venues.joins("LEFT OUTER JOIN venue_events ON venues.id = venue_events.venue_id")
-			@venues = @venues.order("venues.id, venue_events.start_time asc")
-			@venues = @venues.order("venue_events.start_time asc")
+			@subquery = Venue.select("DISTINCT ON(venues.id) venues.name as venue_name, venues.phone, venues.file_name, venue_events.id, venue_events.name as venue_event_name, venue_events.description as venue_event_description, venue_events.start_time")		
+			@subquery = @subquery.joins("LEFT OUTER JOIN venue_events ON venues.id = venue_events.venue_id")
+			@subquery = @subquery.order("venues.id, venue_events.start_time asc")
+			@venues = Arel::SelectManager.new(Arel::Table.engine, Arel.sql("(#{@subquery.to_sql}) as subquery"))
+			@venues = @venues.select("*").order("subquery.start_time asc")
+			
+
+			
 			end
 
 		# if latitude and longitude parameters are available, show distance from venues to user
