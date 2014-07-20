af686101f8b77431f118db2dd3e0f855ef9203ec
Vee Li <michaelvli@yahoo.com>
2014-07-20 15:20:18 -0500

    added mobile splash, home, sort and filter, and video play

--- app/assets/javascripts/altoids.js
+++ app/assets/javascripts/altoids.js
@@ -15,12 +15,20 @@ function get_cookie ( cookie_name ) // Need to define get_cookie at beginning si
 {
     // http://www.thesitewizard.com/javascripts/cookies.shtml
     var cookie_string = document.cookie ;
+
     if (cookie_string.length != 0) {
         var cookie_value = cookie_string.match (
                         '(^|;)[\s]*' +
                         cookie_name +
                         '=([^;]*)' );
-        return decodeURIComponent ( cookie_value[2] ) ;
+		if (cookie_value != null)
+		{
+			return decodeURIComponent ( cookie_value[2] ) ;
+		}
+		else
+		{
+			return '' ;
+		}
     }
     return '' ;
 }
@@ -33,27 +41,55 @@ if (get_cookie( 'device_size' ) == '') // if user doesn't have a cookie indicati
 }
 else
 {
+
 //  TESTING COOKIES FOR SCREEN SIZE
 //delete_cookie('device_size');
 
 	$(function() { //on DOM ready
 //		alert("DOM ready");
-			if ($('.carousel').length)
+
+			// load jquery used for mobile version
+			if (get_cookie( 'device_size' ) == 'xs')
+//			if (get_cookie( 'device_size' ) == 'md' || get_cookie( 'device_size' ) == 'xs') // for development only
+			{
+				show_sort_filter_panel(); // controls display of sort & filter panel
+				
+				$('.slide-left').on('click', function(){ // slides screen content to the left to reveal menu options
+					var slideContent = $('.slide-content');
+					slideContent.animate({
+						// slideContent.outerWidth() : width of .slide-content (including padding and margin)
+						// $(this).outerWidth() - 15*2 : width of navbar-toggle button + left margin (15px) + right margin (15px)
+						// parseInt : parses a string and returns an integer.  In this case, the '10' is the radix indicating use of the decimal system
+				//		left: parseInt(slideContent.css('left'),10) == 0 ? - (slideContent.outerWidth()-$(this).outerWidth() - 15*2):0 //Ternary operator: "If the left css property equals zero, move the element to the left as many pixels as it is wide (including padding and border), otherwise, move it back to zero"
+						left: parseInt(slideContent.css('left'),10) == 0 ? - (slideContent.outerWidth() - 30 ):0 //Ternary operator: "If the left css property equals zero, move the element to the left as many pixels as it is wide (including padding and border), otherwise, move it back to zero"
+					});
+				});
+			}
+
+			if ($('div.alert').length) // fading out flash message alerts
+			{
+				setTimeout(function(){
+					$("div.alert").fadeTo(5000, 0).slideUp(500, function(){
+						$(this).remove();
+					});
+				});	
+			}
+			
+			if ($('.carousel').length) // defining carousel interval and play/pause videos in carousel
 			{
 				carousel_behavior();
 			}
-			
-			if ($('.dotdotdot').length)
+
+			if ($('.dotdotdot').length) // adding ellipsis to end of truncated text
 			{
 				limit_captions(function(){
-					$( ".active.item" ).each(function( index ) {
+					$( ".active.item" ).each(function( index ) { // callback for remove .active class to all items in carousel except the first - allows dotdotdot to complete execution first
 						if(index != 0){
 							$(this).removeClass('active');
 						}
 					});
 				});
-			}	
-
+			}
 			
 			if ($('#venue_event_start_date').length)
 			{
@@ -62,10 +98,11 @@ else
 			
 			if ($('#endless_list').length)
 			{
+
 //	TESTING GEOLOCATION PERMISSION MODAL
 //localStorage.clear();
 //sessionStorage.clear();
-
+/* The case where checkLocalStorage('geolocation_permissions') == 'undefined' was not working for Chrome mobile - missing geolocation_permissions modal???
 				switch(checkLocalStorage('geolocation_permissions')) // Check to see if geolocation permissions modal should be displayed.  LocalStorage variables are stored as text
 				{
 					case 'undefined': // user is shown geolocation permissions modal unless he has clicked "Yes" or "Not Now" buttons in a previous session.
@@ -85,7 +122,19 @@ else
 						);
 						break;
 				}
+*/
+						var user_location = html5_geolocation();
+						user_location.initiate( // calls html5 geolocation to access user location
+							function(){ // anonymous callback function to ensure that user location info from client is retrieved before sending info server via ajax
+								get_list('home','event', user_location.latitude(), user_location.longitude());	// load venues - passing user's latitude and longitude to server 
+							},
+							function(){ // gets list of venues immediately in case user doesn't set geolocation permission in browser
+								get_list('home','event', '', '');	// load venues without passing user's latitude and longitude to server 
+							}
+						);
+				
 				sort_list(); // bind sort buttons
+				video_behavior(); // binds play button to thumbnail videos
 			}
 			
 			if ($('.pagination').length)
@@ -103,6 +152,8 @@ Additional page events are needed or else Turbolinks may prevent some functions 
  */
 
 function page_load_functions(){
+//	alert('5');			
+
 	if ($('#venue_event_start_date').length)
 	{
 		calendar_datepicker(); // plugin
@@ -111,11 +162,17 @@ function page_load_functions(){
 	{
 		endless_scroll();
 	}
-	if ($('.dotdotdot').length)
-	{
-//alert('2');
-//		limit_captions(); // plugin
-	}
+	if ($('.dotdotdot').length) // adding ellipsis to end of truncated text
+			{
+				limit_captions(function(){
+					$( ".active.item" ).each(function( index ) { // callback for remove .active class to all items in carousel except the first - allows dotdotdot to complete execution first
+						if(index != 0){
+							$(this).removeClass('active');
+						}
+					});
+				});
+			}
+
 }
 
 function page_update_functions(){
@@ -153,6 +210,21 @@ function setScreenSizeCookie(callback){
 	callback();
 }
 
+
+
+/* *****
+show_sort_filter_panel - displays sort & filter panel on home page when user clicks on the "filter" button (in footer for mobile page).
+*/
+function show_sort_filter_panel(){
+	$('#show-filters, #cancel-filters, #apply-filters').click(function(){
+		$('.show-filters-fixed-bottom, .apply-filters-fixed-bottom, #filter-container, #pagination-links, #endless_list').slideToggle(200);
+		$('html, body').animate({ scrollTop: 0 }, 0);
+	});
+	
+//	$('#apply-filters').click(function(){
+//		$('form').submit();
+//	});
+}
 
 
 /* *****
@@ -333,36 +405,65 @@ carousel_behavior - defines carousel behavior in terms of 1)frequency of carouse
 function carousel_behavior(){
 	// Bootstrap plugin that controls the interval for advancing the carousel - http://getbootstrap.com/javascript/#carousel
 	$('.carousel').carousel({
-		interval: 10000
+		interval: 5000
 	});
 	
-	// start playing first video
-	$('div.carousel-inner').find('.active').children('video').get(0).play();
-	
-	// start/pause videos based on when carousel slides
-	$('.carousel').on('slid.bs.carousel', function () {
-//		alert("slide method completed");
+	if ($('video').length) // mobile view template for carousel uses thumbnails instead of video
+	{
+
+		// start playing first video
+		$('div.carousel-inner').find('.active').children('video').get(0).play();
 		
-		/* 	play(); active video
-			1) Find the descendants of 'div.carousel-inner' that have the class, 'active'. "Find" method is recursive, looking at all levels below
-			2) Get the descendants of the elements returned from step #1.  "Children" method looks only one level deep, making if faster than the "find" method.
-			3) Get the DOM element of the jQuery object returned from step #2.  In this case, the element is an HTML video element.  A jQuery object is an array-like structure of DOM elements.
-			4) Play is a method that is used on DOM elements (not jQuery objects).  The HTML video element is played.
-		*/
-		$('div.carousel-inner').find('.active').children('video').get(0).play(); 
-		
-		/* 	pause(); all inactive videos
-			1) Find the descendants of 'div.carousel-inner' that have the class, 'item'. "Find" method is recursive, looking at all levels below
-			2) Get the descendants of the elements returned from step #1.
-			3) From the elements returned from step #2, remove those that have the class, 'active'.
-			4) Because there may be multiple elements from step #3, cycle through each of them to perform the following:
-				a) "Children" method looks only one level deep for descendents with the video tag
-				b) Get the DOM element of the jQuery object returned from step #a.  In this case, the element is an HTML video element.  A jQuery object is an array-like structure of DOM elements.
-				c) Pause is a method that is used on DOM elements (not jQuery objects).  The HTML video element is paused.
-		*/
-		$('div.carousel-inner').find('.item').not('.active').each(function(){ 
-			$(this).children("video").get(0).pause();											
+		// start/pause videos based on when carousel slides
+		$('.carousel').on('slid.bs.carousel', function () {
+	//		alert("slide method completed");
+			
+			/* 	play(); active video
+				1) Find the descendants of 'div.carousel-inner' that have the class, 'active'. "Find" method is recursive, looking at all levels below
+				2) Get the descendants of the elements returned from step #1.  "Children" method looks only one level deep, making if faster than the "find" method.
+				3) Get the DOM element of the jQuery object returned from step #2.  In this case, the element is an HTML video element.  A jQuery object is an array-like structure of DOM elements.
+				4) Play is a method that is used on DOM elements (not jQuery objects).  The HTML video element is played.
+			*/
+			$('div.carousel-inner').find('.active').children('video').get(0).play(); 
+			
+			/* 	pause(); all inactive videos
+				1) Find the descendants of 'div.carousel-inner' that have the class, 'item'. "Find" method is recursive, looking at all levels below
+				2) Get the descendants of the elements returned from step #1.
+				3) From the elements returned from step #2, remove those that have the class, 'active'.
+				4) Because there may be multiple elements from step #3, cycle through each of them to perform the following:
+					a) "Children" method looks only one level deep for descendents with the video tag
+					b) Get the DOM element of the jQuery object returned from step #a.  In this case, the element is an HTML video element.  A jQuery object is an array-like structure of DOM elements.
+					c) Pause is a method that is used on DOM elements (not jQuery objects).  The HTML video element is paused.
+			*/
+			$('div.carousel-inner').find('.item').not('.active').each(function(){ 
+				$(this).children("video").get(0).pause();											
+			});
 		});
+	}	
+}
+
+
+
+/* *****
+video_behavior - plays, unhides, hides videos from thumbnails
+*/
+function video_behavior(){
+	//  use selector within "on" method to bind to dynamically retrieved (via AJAX) play buttons found in "#endless_list" container
+	$('#endless_list').on('click', '.glyphicon-play-circle', function(){
+		var selected_image = $(this).siblings('.thumbnail').first().children('img');
+		var selected_video = $(this).siblings('video').first();
+		var video_listener = selected_video.get(0);
+
+	// if user pauses/stops video, 1) display image, 2)hide video, 3)restart carousel
+	video_listener.addEventListener("pause", function() { 
+//		alert("paused has been triggered"); 			
+		selected_image.css("display", "block");
+		selected_video.css("display", "none");
+	}, true);
+	//  Display the video, hide the image, play the video
+	selected_video.css('display', 'block');
+	selected_image.css('display', 'none');
+	selected_video.get(0).play();		
 	});
 }
 
@@ -446,6 +547,7 @@ jQuery.dotdotdot - https://github.com/BeSite/jQuery.dotdotdot
 Shortens captions by appending three periods
 */
 function limit_captions(callback){
+//alert("Capitions are screwing up because pictures haven't been fully loaded yet by the time dotdotdot executes - need to call dotdotdot after pictures have been loaded");
 	$('.dotdotdot').dotdotdot({
         // configuration goes here
     });
